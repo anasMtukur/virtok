befc0d48a033580a5a9895edbff0eebf24791b2f
anasMtukur <anasmtukur91@gmail.com>
2021-10-29 18:27:13 +0800

    First push

---
+++ src/test/java/com/dottedi/virtok/VirtokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dottedi.virtok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
